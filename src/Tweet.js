c5c9fe0eb2e63ded3615f4ce74dd28f3eab2a1b2
salih <slh32064@gmail.com>
2020-11-19 17:49:52 +0530

    fi

---
+++ src/Tweet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Tweet() {
+    return(
+        <div>
+            <h1>This is my Tweet</h1>
+            <h2>This is my content in my Tweet</h2>
+            <h3>This is no of likes I got </h3>
+                    </div>
+    );
+}
+export default Tweet;
